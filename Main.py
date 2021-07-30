9a0a8b30e194c9dd592ca6c1684989e8551787ca
carloscobian96 <carlosjcobian@gmail.com>
2021-07-29 22:38:42 -0400

    Iterable parsing
    Data is being parsed into variables and displayed as a functional string representative of each record.

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import os
 import inquirer
-import Types
+import Type
 import argparse
-
+import struct
 
 path = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,28 +24,54 @@ answers = inquirer.prompt(questions)
 for answer in answers['file']:
     # logger.debug(f"Loading URL's From: {answer}")
     print(f"Selected File: {answer}")
-     # try{
-# -- Header --
-print(f"Header: {str()}")
-magicstring ="MPS7"
-print(f"String: {str.encode(magicstring)}")
-b : bytes = bytes(magicstring, encoding="raw_unicode_escape")
-print(f"String: {b}")
-print(f"Version: {str()}")
-print(f"Header defined Length: {str()}") 
-print(f"total credit amount={str()}")
+    try:
+        # -- Header --
+        file = open(answer, "rb")
+        magicString = file.read(4) 
+        print(f"Magic String: {magicString}")
 
-# for(Header defined Length): #TODO
-   
-#-- Record--
-type = Types.getType(b'\x00')
-print(f"Type: {type}")
-print(f"Timestamp: {str()}")
-print(f"userID: {str()}")
+        version = int.from_bytes(  file.read(1) , 
+                                byteorder='big', 
+                                signed=False)
+        print(f"Version: {version}")
 
-print(f"total credit amount={str()}")
-print(f"total debit amount={str()}")
-print(f"autopays started={str()}")
-print(f"autopays ended={str()}")
-print(f"balance for user {str()}={str()}")
-    # }catch InvalidDataException as e {}
+        records = int.from_bytes(file.read(4), 
+                                byteorder='big', 
+                                signed=False) 
+        print(f"Header defined Length: {records}") 
+
+        #-- Record--
+        for i in list(range(records)):
+
+            typeByte = file.read(1)
+            type = Type.getType(typeByte)
+            # print(f"Type: {type}")
+
+            timestamp = int.from_bytes( file.read(4), 
+                                        byteorder='big', 
+                                        signed=False)
+            # print(f"Timestamp: {timestamp}")
+
+            userID = int.from_bytes(file.read(8), 
+                                    byteorder='big', 
+                                    signed=False)
+            # print(f"userID: {userID}")
+           
+            line = f"{type:14} | {timestamp:12} | {userID:22}"
+            if(Type.hasField(typeByte)):
+                ammount = struct.unpack('!d', file.read(8))[0]
+                # print(f"amount in dollars={ammount}")
+                line = f"{line} | {ammount:}"
+
+            print(f"{line}")
+
+        file.close()
+    except Exception as e:
+        print(f"Exception: {str(e)} ")
+
+
+# print(f"total credit amount={byte}")
+# print(f"total debit amount={byte}")
+# print(f"autopays started={byte}")
+# print(f"autopays ended={byte}")
+# print(f"balance for user {byte}={byte}")
--- Types.py
+++
@@ -1,8 +0,0 @@
-def getType(value):
-    types = {
-        b'\x00': "Debit",
-        b'\x01': "Credit",
-        b'\x02': "StartAutopay",
-        b'\x03': "EndAutopay"
-    }
-    return types.get(value, "Invalid State")
