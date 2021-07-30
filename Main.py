50308b98f2ba55697f386e6c5208342a5dbe8e01
carloscobian96 <carlosjcobian@gmail.com>
2021-07-30 16:04:04 -0400

    Created model
    Parses bytes into objects

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
+from Models import Record,Type
 import os
 import inquirer
-import Type
 import argparse
 import struct
 
@@ -21,6 +21,7 @@ questions = [
             ]
 answers = inquirer.prompt(questions)
 
+recordList:Record = []
 for answer in answers['file']:
     # logger.debug(f"Loading URL's From: {answer}")
     print(f"Selected File: {answer}")
@@ -44,31 +45,42 @@ for answer in answers['file']:
         for i in list(range(records)):
 
             typeByte = file.read(1)
-            type = Type.getType(typeByte)
+            # type = Type.getType(typeByte)
             # print(f"Type: {type}")
 
-            timestamp = int.from_bytes( file.read(4), 
-                                        byteorder='big', 
-                                        signed=False)
+            timestamp = file.read(4)
+            # timestamp = int.from_bytes( file.read(4), 
+            #                             byteorder='big', 
+            #                             signed=False)
             # print(f"Timestamp: {timestamp}")
 
-            userID = int.from_bytes(file.read(8), 
-                                    byteorder='big', 
-                                    signed=False)
+            userID = file.read(8)
+            # userID = int.from_bytes(file.read(8), 
+            #                         byteorder='big', 
+            #                         signed=False)
             # print(f"userID: {userID}")
            
-            line = f"{type:14} | {timestamp:12} | {userID:22}"
+            # line = f"{type:14} | {timestamp:12} | {userID:22}"
+            ammount = None
             if(Type.hasField(typeByte)):
                 ammount = struct.unpack('!d', file.read(8))[0]
                 # print(f"amount in dollars={ammount}")
-                line = f"{line} | {ammount:}"
+                # line = f"{line} | {ammount:}"
 
+            r = Record  (   None,
+                            typeByte,
+                            timestamp,
+                            userID,
+                            ammount
+                        )
+            recordList.append(r)
+
+            line = f"{r.type_idtype.name:14} | {r.timestamp:12} | {r.user_iduser.iduser:22} | {r.ammount}"
             print(f"{line}")
 
         file.close()
     except Exception as e:
         print(f"Exception: {str(e)} ")
-
 
 # print(f"total credit amount={byte}")
 # print(f"total debit amount={byte}")
